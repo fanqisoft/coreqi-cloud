ad3519c5d720441acd3055a4e515a82843c8175e
fanqi <fanqisoft@gmail.com>
2020-03-09 19:09:57 +0800

    同步跟踪视频进度13

---
+++ cloud-consumer-order80/src/main/java/cn/coreqi/springcloud/entities/Payment.java
@@ -0,0 +1,15 @@
+package cn.coreqi.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Payment {
+    private Long id;
+    private String serial;
+}
