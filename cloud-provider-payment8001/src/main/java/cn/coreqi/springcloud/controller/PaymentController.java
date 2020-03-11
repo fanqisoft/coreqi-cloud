e62b57305a29eb2e580f13f4d171779810e0075d
fanqi <fanqisoft@gmail.com>
2020-03-11 15:17:55 +0800

    同步跟踪视频进度22 为服务提供者的接口响应添加测试内容

--- cloud-provider-payment8001/src/main/java/cn/coreqi/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/coreqi/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import cn.coreqi.springcloud.core.CommonResult;
 import cn.coreqi.springcloud.entities.Payment;
 import cn.coreqi.springcloud.services.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,14 +16,17 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;  //用于测试在负载的情况下由那个服务提供者提供的服务
+
     @PostMapping(value = "/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("******插入结果： " + result);
         if(result > 0){
-            return  new CommonResult(200,"插入数据库成功",result);
+            return  new CommonResult(200,"插入数据库成功,serverPort: " + serverPort,result);
         }else{
-            return  new CommonResult(400,"插入数据库失败",null);
+            return  new CommonResult(400,"插入数据库失败,serverPort: " + serverPort,null);
         }
     }
 
@@ -31,9 +35,9 @@ public class PaymentController {
         Payment result = paymentService.getPaymentById(id);
         log.info("******查询结果： " + result);
         if(result != null){
-            return  new CommonResult(200,"查询成功",result);
+            return  new CommonResult(200,"查询成功,serverPort: " + serverPort,result);
         }else{
-            return  new CommonResult(400,"没有对应记录，查询Id： " + id,null);
+            return  new CommonResult(400,"没有对应记录，查询Id： " + id + ",serverPort: " + serverPort,null);
         }
     }
 }
