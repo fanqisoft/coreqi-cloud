da08c13181990c816af150d50459ca44298d1475
fanqi <fanqisoft@gmail.com>
2020-03-11 19:39:41 +0800

    同步跟踪视频进度24 为8001添加服务发现

--- cloud-provider-payment8001/src/main/java/cn/coreqi/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/coreqi/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import cn.coreqi.springcloud.entities.Payment;
 import cn.coreqi.springcloud.services.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,6 +18,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;  //用于测试在负载的情况下由那个服务提供者提供的服务
@@ -39,5 +45,21 @@ public class PaymentController {
         }else{
             return  new CommonResult(400,"没有对应记录，查询Id： " + id + ",serverPort: " + serverPort,null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();	//获取所有在Eureka中注册的服务名称列表
+        for(String service : services){
+            log.info("serviceName: " + service);
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);	//根据服务名获取服务的详细信息
+            for(ServiceInstance instance : instances){
+                log.info("服务Id: " + instance.getServiceId());
+                log.info("服务主机: " + instance.getHost());
+                log.info("服务端口: " + instance.getPort());
+                log.info("服务Uri: " + instance.getUri());
+            }
+        }
+        return this.discoveryClient;
     }
 }
