bcccbdbf759346e16157030bd8252da0e330cbd9
fanqi <fanqisoft@gmail.com>
2020-03-16 19:46:08 +0800

    同步跟踪视频进度60 为Hystrix8001添加服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/cn/coreqi/springcloud/services/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/coreqi/springcloud/services/PaymentService.java
@@ -1,6 +1,9 @@
 package cn.coreqi.springcloud.services;
 
+import org.springframework.web.bind.annotation.PathVariable;
+
 public interface PaymentService {
     String paymentInfo_OK(Integer id);
     String paymentInfo_Timeout(Integer id);
+    String paymentCircuitBreaker(Integer id);
 }
