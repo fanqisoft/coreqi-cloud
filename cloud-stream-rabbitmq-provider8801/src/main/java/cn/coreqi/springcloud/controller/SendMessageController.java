673d1f5688bf0d9053faf70026c7181fc85c5b57
fanqi <fanqisoft@gmail.com>
2020-03-20 12:59:39 +0800

    同步跟踪视频进度87 添加Stream消息驱动生产者

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/cn/coreqi/springcloud/controller/SendMessageController.java
@@ -0,0 +1,19 @@
+package cn.coreqi.springcloud.controller;
+
+import cn.coreqi.springcloud.services.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+}
