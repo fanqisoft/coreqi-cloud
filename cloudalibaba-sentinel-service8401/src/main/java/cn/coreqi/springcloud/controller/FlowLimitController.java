40751d676cd73a0905a59cb5039d2de87dfee466
fanqi <fanqisoft@gmail.com>
2020-03-23 13:19:42 +0800

    同步跟踪视频进度121 同步最新代码，更新笔记文档

--- cloudalibaba-sentinel-service8401/src/main/java/cn/coreqi/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/coreqi/springcloud/controller/FlowLimitController.java
@@ -1,9 +1,13 @@
 package cn.coreqi.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -14,5 +18,17 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB(){
         return  "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD(){
+        //暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "-----testD";
     }
 }
