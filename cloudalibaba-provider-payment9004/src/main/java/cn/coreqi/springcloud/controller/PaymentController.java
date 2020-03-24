a9b3825baa4fadf599d8633bdf8756af54a47e2d
fanqi <fanqisoft@gmail.com>
2020-03-24 17:58:49 +0800

    同步跟踪视频进度135

---
+++ cloudalibaba-provider-payment9004/src/main/java/cn/coreqi/springcloud/controller/PaymentController.java
@@ -0,0 +1,33 @@
+package cn.coreqi.springcloud.controller;
+
+import cn.coreqi.springcloud.core.CommonResult;
+import cn.coreqi.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    public static HashMap<Long, Payment> hashMap = new HashMap<>();
+
+    static {
+        hashMap.put(1L,new Payment(1L, "71fc3fbe9b9443b18bf1b1eb9c115b59"));
+        hashMap.put(2L,new Payment(2L, "870a3d633b684512a25526e1dfc5076e"));
+        hashMap.put(3L,new Payment(3L, "1c18bf558d1e4530b8b8d7941d5f5e05"));
+    }
+
+    @GetMapping("/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id){
+        Payment payment = hashMap.get(id);
+        CommonResult<Payment> result = new CommonResult<>(200,"from mysql,serverPort: " + serverPort,payment);
+        return result;
+    }
+
+}
